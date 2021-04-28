c8a5e52edfa96df147f700705b8bf9f4476ecd28
Nick Rabb <nick.rabb2@gmail.com>
2021-04-28 17:29:37 -0400

    Fixed joining of data with labels

--- combineJson.py
+++ combineJson.py
@@ -9,24 +9,39 @@ Created on Wed Apr 21 14:58:18 2021
 import pandas as pd
 import os
 
+# 10166101979250354
 
+#foxNews = pd.read_json("news-data/fb-posts/FoxNews.json")
+#foxNews.rename(columns = {"post_id":"native_id"},inplace=True)
 
-foxNews = pd.read_json("news-data/fb-posts/FoxNews.json")
-foxNews.rename(columns = {"post_id":"native_id"},inplace=True)
+result = pd.DataFrame()
 
-dataframes = [foxNews]
+print('Reading in fb post data')
+for filename in os.listdir("news-data/fb-posts/"):
+    if '.json' in filename:
+        file = filename.split(".")
+        file = file[0]
+        filename = "./news-data/fb-posts/"+filename
+        print(filename)
+        data = pd.read_json(filename)
+        data.rename(columns = {"post_id":"native_id"},inplace=True)
+        if "native_id" in data.columns:
+            result = result.append(data)
 
+result = result.set_index('native_id')
+
+print('Reading in label data')
 for filename in os.listdir("labeled-data"):
     file = filename.split(".")
     file = file[0]
-    filename = "media-ideology-coding-main/labeled-data/"+filename
+    filename = "./labeled-data/"+filename
     print(filename)
     data = pd.read_json(filename)
     data.rename(columns={"code":file}, inplace=True)
     if "native_id" in data.columns:
-        dataframes.append(data)
+        result = result.join(data.set_index('native_id'))
 
-result = pd.concat([df.set_index("native_id") for df in dataframes], ignore_index=False)
+#result = pd.concat([df.set_index("native_id") for df in dataframes], ignore_index=False)
 
 """
     try:
