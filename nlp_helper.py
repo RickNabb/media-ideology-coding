d0625b02020f3e56ae1460808edafd8b9ce42fbb
Nick Rabb <nick.rabb2@gmail.com>
2021-10-04 09:35:31 -0400

    Added function to do entity finding as a set

--- nlp_helper.py
+++ nlp_helper.py
@@ -16,6 +16,8 @@ NER label "GROUP"
 '''
 NER_GROUPS = [ 'medical officials', 'doctors', 'democrats', 'republicans', 'immigrants', 'government officials', 'legislators', 'health officials', 'communists', 'socialists', 'libertarians', 'children', 'parents', 'elders', 'the elderly', 'Americans', 'patriots', 'governors', 'legislators', 'migrants', 'terrorists', 'the public', 'citizens', 'epidemiologists', 'representatives', 'congress', 'hospital workers', 'scientists', 'mayors', 'celebrities', 'the media' ]
 
+# Maybe somehow these should get put into BERT vectors so we could find similar enough synonyms and not have to hand code all of them...? As long as they are some epsilon threshold away in BERT space and contain similar words?
+
 def label_ner_groups(doc, groups):
   '''
   Given a spacy doc object, search it for instances of any of the groups passed
@@ -23,6 +25,8 @@ def label_ner_groups(doc, groups):
 
   :param doc: A spacy doc object.
   :param groups: A list of group strings to label with the NER GROUP label.
+
+  Mutates the doc object so there's no need to return
   '''
   doc_text = [ token.text.lower() for token in doc ]
   entities = []
@@ -40,6 +44,19 @@ def label_ner_groups(doc, groups):
       ent = Span(doc, i, i+len(group_tokens), label=NER_GROUP_LABEL)
       entities.append(ent)
   doc.set_ents(entities, default="unmodified")
+
+def find_entity_set(nlp, text):
+  '''
+  Get a set of all of the entities mentioned in a piece of text.
+
+  :param nlp: The spacy parser to use.
+  :param text: The text to search for entities.
+  '''
+  nlp = spacy.load('en_core_web_sm')
+  doc = nlp(text)
+  label_ner_groups(doc, NER_GROUPS)
+  entities = doc.ents
+  return set(entities)
 
 def split_into_sentences(text):
   '''
